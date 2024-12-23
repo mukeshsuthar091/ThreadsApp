5a75c39434d45d2713ec4d725c09ac9699d3c929
mukesh <sutharmukesh910@gmail.com>
2024-12-23 21:23:46 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Button, Container } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+
+  return (
+    <Container maxW='620px'>
+      <Routes>
+        <Route path="/:username" element={<UserPage />}/>
+      </Routes>
+    </Container>
+  )
+}
+
+export default App
